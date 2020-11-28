1a63c9638ab4e0e8cdf5c11d20d3cb0375d18655
princesuwal <princesuwal@gmail.com>
2020-11-28 17:54:26 -0500

    Psuwal Login/Register Commit

---
+++ app/src/main/java/com/example/aplus/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.aplus;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class MainActivity extends FragmentActivity {
+    private static final int NUM_PAGES = 2;
+
+    private ViewPager2 viewPager;
+
+    private FragmentStateAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager);
+        pagerAdapter = new ScreenSlidePagerAdapter(this);
+        pagerAdapter.createFragment(0);
+        pagerAdapter.createFragment(1);
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(viewPager.getCurrentItem() == 0){
+            super.onBackPressed();
+        }else{
+            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+        }
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStateAdapter{
+        public ScreenSlidePagerAdapter(FragmentActivity fa){
+            super(fa);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position){
+            Fragment newFragment;
+            switch (position){
+                case 0:
+                    newFragment = new loginFragment();
+                    break;
+                case 1:
+                    newFragment = new registerFragment();
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + position);
+            }
+            return newFragment;
+        }
+
+        @Override
+        public int getItemCount() {
+            return NUM_PAGES;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/aplus/loginFragment.java
@@ -0,0 +1,25 @@
+package com.example.aplus;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class loginFragment extends Fragment {
+
+
+    public loginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+}
