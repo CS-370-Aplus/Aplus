1c6d1398ff5bcfa0014b6067eff5b237155d0ab2
niraj970 <nirajlama96@gmail.com>
2020-12-15 17:41:25 -0500

    niraj(profile setup)

--- app/src/main/java/com/example/aplus/home/ui/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/aplus/home/ui/Profile/ProfileFragment.java
@@ -4,13 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.aplus.R;
 
@@ -20,6 +16,6 @@ public class ProfileFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
 
 
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
